340685fb5fd385c73dd068ebc373cd2147c5059c
Timur Nugmanov <general@nugmanovt.im>
2018-02-19 15:06:41 +0400

    Respond with proper HTTP headers

--- core/basic_controller.rb
+++ core/basic_controller.rb
@@ -1,9 +1,23 @@
+require 'time'
+
 class BasicController
   def initialize(headers)
-    @headers = headers
+    @request_headers = headers
   end
 
   def render(path)
-    IO.read(path)
+    @response_body = IO.read(path)
+    "#{response_headers('200 OK')}\n#{@response_body}"
+  end
+
+  private
+
+  def response_headers(status)
+    "HTTP/1.1 #{status}\n"\
+    "Content-Type: text/html; charset=utf-8\n"\
+    "Cache-Control: no-cache\n"\
+    "Date: #{Time.now.rfc822}\n"\
+    "Server: TestServer\n"\
+    "Content-Length: #{@response_body.bytesize}\n"
   end
 end
